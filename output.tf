21ff0244f463fed598321d8679ee333d392ba8da
Siva <sivakumarpulis@gmail.com>
2025-04-30 14:39:17 +0530

    terraform_aws_vpc

--- output.tf
+++ output.tf
@@ -2,14 +2,29 @@ output "az-zones" {
   value = data.aws_availability_zones.available.names
 }
 
-output "public_subnet" {
+output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-output "private_subnet" {
+output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
-output "database_subnet" {
+output "database_subnet_ids" {
   value = aws_subnet.database[*].id
+}
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "database_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.gw.id
 }
